9c210a05f940f1ded365796874df250457f68c98
Trevor B <trevor@mtaonline.net>
2015-11-01 23:12:44 -0900

    Set up static middleware, updated about page, added handlebars views

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -2,36 +2,50 @@ var express = require('express');
 
 var app = express();
 
+// set up handlebars view engine
+var handlebars = require('express3-handlebars')
+	.create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
+
+// Set up static folder
+app.use(express.static(__dirname + '/public'));
 
 // custom home page
 app.get('/', function(req, res) {
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+    res.render('home');
 });
+
+var fortunes = [
+    "If you conquer your fears you will conquer the world.",
+    "Rivers not only deliver water, but life.",
+    "No unknown is worth your fear.",
+    "A pleasant surprise awaits you in the near future.",
+    "Whenever possible make things simpler."
+];
 
 // custom about page
 app.get('/about', function(req, res) {
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', { fortune: randomFortune });
 });
 
 // custom 404 page
-app.use(function(req,res) {
-    res.type('text/plain');
+app.use(function(req, res, next) {
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 // custom 500 page
 app.use(function(err, req, res, next) {
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Internal Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function() {
-    console.log('Express started on http://localhost:' + 
+    console.log('Express started on http://localhost:' +
                 app.get('port') + ': press CTRL-C to quit.');
 });
